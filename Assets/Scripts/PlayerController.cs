e1604164fd6e23dc6f739b10f47b8ad9c0c3c2ec
Cas0n-McVey <absolgamer25@gmail.com>
2025-08-26 15:14:56 -0500

    Spawn Manager

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 7.0f;
+    private float speed = 100.0f;
     private float zBound = 10.0f;
     private Rigidbody playerRb;
 
@@ -41,6 +41,22 @@ public class PlayerController : MonoBehaviour
         if (transform.position.z > zBound)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Powerup"))
+        {
+           Destroy(other.gameObject);
         }
     }
 }
