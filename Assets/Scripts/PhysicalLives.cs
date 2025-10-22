9d96cc9c81c0ca46ff6430fc47af2d7b7cfa936e
Cas0n-McVey <absolgamer25@gmail.com>
2025-10-22 15:11:16 -0500

    Smoke and fire

--- Assets/Scripts/PhysicalLives.cs
+++ Assets/Scripts/PhysicalLives.cs
@@ -4,6 +4,11 @@ public class PhysicalLives : MonoBehaviour
 {
     public bool gameOver = false;
 
+    public ParticleSystem smokeParticle;
+    public ParticleSystem smokeyParticle;
+    public ParticleSystem fireParticle;
+    public ParticleSystem explosionParticle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
