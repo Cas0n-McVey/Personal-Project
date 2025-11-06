16f39152661b0d8e36890ef7e665ec4aaae9a372
Cas0n-McVey <absolgamer25@gmail.com>
2025-11-06 15:14:50 -0600

    Skybox and menus

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public Button retryButton;
+    public Button playButton;
+    public Button settingsButton;
+    public Button creditsButton;
+    public Button mainMenuButton;
+    public Button quitButton;
+    public Button exitCreditsButton;
+    public Canvas mainMenu;
+    public GameObject spawnManager;
+    public GameObject player;
 
     private GameOverManager gameOverManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameOverManager = GameObject.Find("Player").GetComponent<GameOverManager>();
+        MainMenu();
     }
 
     // Update is called once per frame
@@ -21,11 +31,31 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-            retryButton.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
     }
 
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void MainMenu()
+    {
+        playButton.gameObject.SetActive(true);
+        settingsButton.gameObject.SetActive(true);
+        creditsButton.gameObject.SetActive(true);
+        quitButton.gameObject.SetActive(true);
+        mainMenu.gameObject.SetActive(true);
+        spawnManager.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        playButton.gameObject.SetActive(false);
+        settingsButton.gameObject.SetActive(false);
+        creditsButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(false);
+        spawnManager.SetActive(true);
     }
 }
